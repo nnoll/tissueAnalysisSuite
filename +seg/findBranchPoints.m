bd80ca638720f7c738faf716113566f53b66f129
Noah Mitchell <noah.prentice.mitchell@gmail.com>
2022-04-23 15:52:20 -0700

    streamline findBranchPoints.m and find_vertices.m

--- +seg/findBranchPoints.m
+++ +seg/findBranchPoints.m
@@ -1,12 +1,21 @@
-function [ branchPoints ] = findBranchPoints( skel )
-    
-    branchPoints = zeros(size(skel));
-    rows = 2:(size(skel,1)-1);
-    cols = 2:(size(skel,2)-1);
-    branchPoints(rows,cols) = skel(rows+1,cols) + skel(rows-1,cols) + ...
-                              skel(rows,cols+1) + skel(rows,cols-1);
-    branchPoints = branchPoints .* skel;
-    branchPoints = branchPoints >= 3;
+function [ branchPoints, bxy ] = findBranchPoints( skel )
+
+% Original version by Nick Noll
+% branchPoints = zeros(size(skel));
+% rows = 2:(size(skel,1)-1);
+% cols = 2:(size(skel,2)-1);
+% branchPoints(rows,cols) = skel(rows+1,cols) + skel(rows-1,cols) + ...
+%                           skel(rows,cols+1) + skel(rows,cols-1);
+% branchPoints = branchPoints .* skel;
+% branchPoints = branchPoints >= 3;
+
+% New version (NPM / Matlab builtin)
+branchPoints = bwmorph(skel, 'branchpoints') ;
+
+if nargout > 1
+    [y,x] = find(branchPoints) ;
+    bxy = [x(:), y(:)] ;
+end
 
 end
 
--- +seg/find_vertices.m
+++ +seg/find_vertices.m
@@ -1,91 +1,99 @@
-function [ Vdat ] = find_vertices( L, threeFold, very_far)
-    %
-    % L : 
-    % threeFold : 
-    % very_far : float
-    %   threshold maximum distance to consider a pair of cells to be
-    %   adjacent
-    % 
-    
-    %Find position of all vertices
-    if (nargin == 1 || threeFold == 0)
-        CC = bwconncomp(seg.findBranchPoints( L==0 ), 8);
+function [ Vdat ] = find_vertices( L, threeFold, very_far, connectivity)
+%
+% Parameters
+% ----------
+% L : segmentation image
+%   L(i,j) returns the index of the segmented object
+% threeFold : 
+%   
+% very_far : float
+%   threshold maximum distance to consider a pair of cells to be
+%   adjacent
+% connectivity : 4 or 8 or higher (default=8)
+%
 
-        S = regionprops(CC,'Centroid');
-        Vertices = cell(length(S),1);
-        for ii = 1:length(S)
-            Vertices{ii} = round(S(ii).Centroid);
-        end
-    else
-%         rgb(:,:,1) = (L==0);
-%         rgb(:,:,2) = seg.findBranchPoints(L==0);
-%         rgb(:,:,3) = 0;
-%         imshow(uint8(255*rgb))
-%         pause
-        [y,x] = find(seg.findBranchPoints(L==0));
-        Vertices = cell(length(x),1);
-        for ii = 1:length(y)
-            Vertices{ii} = [x(ii),y(ii)];
-            CC.PixelIdxList{ii} = y(ii) + size(L,1)*(x(ii)-1);
-        end
-        
+if nargin < 4
+    connectivity = 8 ;
+end
+
+%Find position of all vertices
+if (nargin == 1 || threeFold == 0)
+    CC = bwconncomp(seg.findBranchPoints( L==0 ), connectivity);
+
+    S = regionprops(CC,'Centroid');
+    Vertices = cell(length(S),1);
+    for ii = 1:length(S)
+        Vertices{ii} = round(S(ii).Centroid);
     end
-        
-    %Find neighboring cells of vertices.
-    for v = 1:length(Vertices)
-        nCells = 9*length(CC.PixelIdxList{v});
-        for ii = 1:length(CC.PixelIdxList{v})
-            nCells((9*(ii-1)+1):(9*ii)) = CC.PixelIdxList{v}(ii) + ...
-            [0,-1,1,size(L,1),size(L,1)-1,size(L,1)+1,-size(L,1),-size(L,1)-1,-size(L,1)+1];
-        end
-        nCells = L(nCells);
-        nCells = unique(nCells(nCells~=0));
-        
+else
+    %         rgb(:,:,1) = (L==0);
+    %         rgb(:,:,2) = seg.findBranchPoints(L==0);
+    %         rgb(:,:,3) = 0;
+    %         imshow(uint8(255*rgb))
+    %         pause
+    [y,x] = find(seg.findBranchPoints(L==0));
+    Vertices = cell(length(x),1);
+    for ii = 1:length(y)
+        Vertices{ii} = [x(ii),y(ii)];
+        CC.PixelIdxList{ii} = y(ii) + size(L,1)*(x(ii)-1);
+    end
+end
+
+%Find neighboring cells of vertices.
+for v = 1:length(Vertices)
+    nCells = 9*length(CC.PixelIdxList{v});
+    for ii = 1:length(CC.PixelIdxList{v})
+        nCells((9*(ii-1)+1):(9*ii)) = CC.PixelIdxList{v}(ii) + ...
+        [0,-1,1,size(L,1),size(L,1)-1,size(L,1)+1,-size(L,1),-size(L,1)-1,-size(L,1)+1];
+    end
+    nCells = L(nCells);
+    nCells = unique(nCells(nCells~=0));
+
 %         if (length(nCells) == 2)
 %             imshow(imdilate(L==0,strel('disk',2)))
 %             hold all
 %             scatter(Vertices{v}(1),Vertices{v}(2))
 %             pause
 %         end
-            
-        Vertices{v} = horzcat(uint16(Vertices{v}),uint16(nCells));
-    end
-    
-    N = length(Vertices);
-    adj = zeros(N,N);
-    Vdat(N) = struct('vertxcoord',[],'vertycoord',[],'ncells',[],'nverts',[]);
 
-    %Extract out neighboring cells.
-    R = zeros(2,N);
-    for ii=1:N
-        Vdat(ii).vertxcoord = Vertices{ii}(1);
-        Vdat(ii).vertycoord = Vertices{ii}(2);
-        R(1,ii) = Vertices{ii}(1);
-        R(2,ii) = Vertices{ii}(2);
-        Vdat(ii).ncells = Vertices{ii}(3:end);
-    end
+    Vertices{v} = horzcat(uint16(Vertices{v}),uint16(nCells));
+end
 
-    D = bsxfun(@plus,dot(R,R,1)',dot(R,R,1)) - 2*(R'*R);
-    %Compute vertex connections via overlap of neighboring cells.
-    for ii = 1:N
-        for jj = ii+1:N
-           if (D(ii,jj) <= very_far^2)
-               common_mems = ismembc(Vdat(ii).ncells,Vdat(jj).ncells);
-               ncommon = sum(common_mems);
-               if (ncommon >= 2)
-                   adj(ii,jj) = 1;
-                   adj(jj,ii) = 1;
-               end
+N = length(Vertices);
+adj = zeros(N,N);
+Vdat(N) = struct('vertxcoord',[],'vertycoord',[],'ncells',[],'nverts',[]);
+
+%Extract out neighboring cells.
+R = zeros(2,N);
+for ii=1:N
+    Vdat(ii).vertxcoord = Vertices{ii}(1);
+    Vdat(ii).vertycoord = Vertices{ii}(2);
+    R(1,ii) = Vertices{ii}(1);
+    R(2,ii) = Vertices{ii}(2);
+    Vdat(ii).ncells = Vertices{ii}(3:end);
+end
+
+D = bsxfun(@plus,dot(R,R,1)',dot(R,R,1)) - 2*(R'*R);
+%Compute vertex connections via overlap of neighboring cells.
+for ii = 1:N
+    for jj = ii+1:N
+       if (D(ii,jj) <= very_far^2)
+           common_mems = ismembc(Vdat(ii).ncells,Vdat(jj).ncells);
+           ncommon = sum(common_mems);
+           if (ncommon >= 2)
+               adj(ii,jj) = 1;
+               adj(jj,ii) = 1;
            end
-        end
-        Vdat(ii).nverts = find(adj(ii,:)==1);
+       end
     end
+    Vdat(ii).nverts = find(adj(ii,:)==1);
+end
 
-    for ii = 1:length(Vdat)
-        if (length(Vdat(ii).nverts) == 4)
-            Vdat(ii).fourfold = 1;
-        end
+for ii = 1:length(Vdat)
+    if (length(Vdat(ii).nverts) == 4)
+        Vdat(ii).fourfold = 1;
     end
+end
   
 end
 
